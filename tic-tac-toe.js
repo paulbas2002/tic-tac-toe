70d40be2d0b4414df56db56a69a64d0c57c943cd
paulbas2002 <paulbas2002@gmail.com>
2016-12-21 15:35:41 -0800

    all done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,124 @@
-// Write all your JavaScript code in this file!
-// You can use the namespace variable that's defined here.
-var namespace = "http://www.w3.org/2000/svg"
-var turn= "player1"
-function topLeft(){
-  if(turn=="player1")
-  var circle = document.createElementNS(namespace, "circle")
-  var canvas = document.getElementById("game-board")
-  circle.setAttribute("cx", 50)
-  circle.setAttribute("cy", 50)
-  circle.setAttribute("r", 30)
-  circle.setAttribute("fill", "purple")
+var turn ="player1"
+function pink1(){
+  if(turn=="player1"){
+    var pink1 = document.getElementById("pink1")
+    pink1.setAttribute("fill", "pink")
+turn="player2"
+}else{
+  var blue1 = document.getElementById("blue1")
+  blue1.setAttribute("fill", "blue")
+  turn="player1"
+}
+}
+
+
+
+var turn ="player1"
+function pink2(){
+  if(turn=="player1"){
+    var pink2 = document.getElementById("pink2")
+    pink2.setAttribute("fill", "pink")
+turn="player2"
+}else{
+  var blue2 = document.getElementById("blue1")
+  blue2.setAttribute("fill", "blue")
+  turn="player1"
+}
+}
+
+
+
+var turn ="player1"
+function pink3(){
+  if(turn=="player1"){
+    var pink3 = document.getElementById("pink3")
+    pink3.setAttribute("fill", "pink")
+turn="player2"
+}else{
+  var blue3 = document.getElementById("blue3")
+  blue3.setAttribute("fill", "blue")
+  turn="player1"
+}
+}
+
+var turn ="player1"
+function pink4(){
+  if(turn=="player1"){
+    var pink4 = document.getElementById("pink4")
+    pink4.setAttribute("fill", "pink")
+turn="player2"
+}else{
+  var blue4 = document.getElementById("blue4")
+  blue4.setAttribute("fill", "blue")
+  turn="player1"
+}
+}
+
+var turn ="player1"
+function pink5(){
+  if(turn=="player1"){
+    var pink5 = document.getElementById("pink5")
+    pink5.setAttribute("fill", "pink")
+turn="player2"
+}else{
+  var blue5 = document.getElementById("blue5")
+  blue5.setAttribute("fill", "blue")
+  turn="player1"
+}
+}
+
+
+var turn ="player1"
+function pink6(){
+  if(turn=="player6"){
+    var pink6 = document.getElementById("pink6")
+    pink5.setAttribute("fill", "pink")
+turn="player2"
+}else{
+  var blue6 = document.getElementById("blue")
+  blue6.setAttribute("fill", "blue")
+  turn="player1"
+}
+}
+
+
+var turn ="player1"
+function pink7(){
+  if(turn=="player1"){
+   var pink7 = document.getElementById("pink7")
+   pink7.setAttribute("fill", "pink")
+turn="player2"
+}else{
+  var blue7 = document.getElementById("blue7")
+  blue7.setAttribute("fill", "blue")
+  turn="player1"
+}
+}
+
+
+var turn ="player1"
+function pink8(){
+  if(turn=="player1"){
+    var pink8 = document.getElementById("pink8")
+    pink8.setAttribute("fill", "pink")
+turn="player2"
+}else{
+  var blue8 = document.getElementById("blue8")
+  blue8.setAttribute("fill", "blue")
+  turn="player1"
+}
+}
+
+
+var turn ="player1"
+function pink9(){
+  if(turn=="player1"){
+    var pink9 = document.getElementById("pink9")
+    pink9.setAttribute("fill", "pink")
+turn="player2"
+}else{
+  var blue9 = document.getElementById("blue9")
+  blue9.setAttribute("fill", "blue")
+  turn="player1"
+}
 }
